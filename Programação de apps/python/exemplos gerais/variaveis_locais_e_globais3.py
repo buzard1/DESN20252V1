be4cfdbcdda928e05b72ae698b005e4c56b649ee
buzard1 <matheusdlanjos@hotmail.com>
2025-02-17 17:12:12 -0300

    17/02/2025

---
+++ Programação de apps/python/exemplos gerais/variaveis_locais_e_globais3.py
@@ -0,0 +1,12 @@
+def f1(a):
+    print(a + x)
+
+def f2(a):
+    x = x + 1
+    print(a + x)
+
+x = 4
+f1(3)
+f2(3)
+
+     
---
+++ Programação de apps/python/exemplos gerais/variaveis_locais_e_globais5.py
@@ -0,0 +1,12 @@
+def f2(a):
+    c = 10
+    print(a + x + c)
+
+
+
+x = 4
+f2(3)
+print(x)
+print(c) #da erro pois a variavel c foi criada dentro da função f2 e ela so existe dentro desta
+
+     
